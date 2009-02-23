eae95c57a785dca5e5a32eb516e2c21a56a5298f
Yoan Blanc <yoan@dosimple.ch>
2009-02-23 17:44:47 +0100

    Restish application comes with bundled example tests for free.
    $ paster create --template=restish foo
    $ cd foo
    $ python setup.py test
    [...]
    tests.test_hello.TestHello.testIndex ... ok
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.046s
    
    OK

--- restish/tests/util.py
+++ restish/tests/util.py
@@ -1,6 +1,6 @@
 def wsgi_out(app, environ):
     out = {}
-    def start_response(status, headers):
+    def start_response(status, headers, exc_info=None):
         out['status'] = status
         out['headers'] = headers
     out['body'] = ''.join(iter(app(environ, start_response)))
