86b7b34a716389ba1116b22ea2a0915a5ee8058f
Yoan Blanc <yoan@dosimple.ch>
2009-03-04 20:42:42 +0100

    Forget those... (no -a...)

--- restish/resource.py
+++ restish/resource.py
@@ -11,6 +11,7 @@ from restish import http, _mimeparse as mimeparse
 _RESTISH_CHILD = "restish_child"
 _RESTISH_METHOD = "restish_method"
 _RESTISH_MATCH = "restish_match"
+_RESTISH_CHILD_CLASS = "restish_child_class"
 
 
 SHORT_CONTENT_TYPE_EXTRA = {
@@ -18,177 +19,36 @@ SHORT_CONTENT_TYPE_EXTRA = {
         }
 
 
-class _metaResource(type):
-    """
-    Resource meta class that gathers all annotations for easy access.
-    """
-    def __new__(cls, name, bases, clsattrs):
-        cls = type.__new__(cls, name, bases, clsattrs)
-        _gather_request_dispatchers(cls, clsattrs)
-        _gather_child_factories(cls, clsattrs)
-        return cls
-
-
-def _gather_request_dispatchers(cls, clsattrs):
-    """
-    Gather any request handler -annotated methods and add them to the class's
-    request_dispatchers attribute.
-    """
-    # Copy the super class's 'request_dispatchers' dict (if any) to this class.
-    cls.request_dispatchers = dict(getattr(cls, 'request_dispatchers', {}))
-    for callable in _find_annotated_funcs(clsattrs, _RESTISH_METHOD):
-        method = getattr(callable, _RESTISH_METHOD, None)
-        match = getattr(callable, _RESTISH_MATCH)
-        cls.request_dispatchers.setdefault(method, []).append((callable, match))
-
-
-def _gather_child_factories(cls, clsattrs):
-    """
-    Gather any 'child' annotated methods and add them to the class's
-    child_factories attribute.
-    """
-    # Copy the super class's 'child_factories' list (if any) to this class.
-    cls.child_factories = list(getattr(cls, 'child_factories', []))
-    # Extend child_factories to include the ones found on this class.
-    child_factories = _find_annotated_funcs(clsattrs, _RESTISH_CHILD)
-    cls.child_factories.extend((getattr(f, _RESTISH_CHILD), f) \
-                               for f in child_factories)
-    # Sort the child factories by score.
-    cls.child_factories = sorted(cls.child_factories, \
-                                 key=lambda i: i[0].score, reverse=True)
-
-
-def _find_annotated_funcs(clsattrs, annotation):
-    """
-    Return a (generated) list of methods that include the given annotation.
-    """
-    funcs = (item for item in clsattrs.itervalues() \
-             if inspect.isroutine(item))
-    funcs = (func for func in funcs \
-             if getattr(func, annotation, None) is not None)
-    return funcs
-
-
-class Resource(object):
-    """
-    Base class for additional resource types.
-
-    Provides the basic API required of a resource (resource_child(request,
-    segments) and __call__(request)), possibly dispatching to annotated methods
-    of the class (using metaclass magic).
-    """
-
-    __metaclass__ = _metaResource
-
-    def resource_child(self, request, segments):
-        for matcher, func in self.child_factories:
-            match = matcher(request, segments)
-            if match is not None:
-                break
-        else:
-            return None
-        match_args, match_kwargs, segments = match
-        result = func(self, request, segments, *match_args, **match_kwargs)
-        if result is None:
-            return None
-        elif isinstance(result, tuple):
-            return result
-        else:
-            return result, segments
-
-    def __call__(self, request):
-        # Get the dispatchers for the request method.
-        dispatchers = self.request_dispatchers.get(request.method)
-        # No normal dispatchers for method found,
-        if dispatchers is None:
-            # Looking for a magic dispatcher
-            dispatchers = self.request_dispatchers.get(ALL.method)
-            # No magic dispatchers found either,
-            # send 405 with list of allowed methods.
-            if dispatchers is None:
-                return http.method_not_allowed(', '.join(self.request_dispatchers))
-        # Look up the best dispatcher
-        dispatcher = _best_dispatcher(dispatchers, request)
-        if dispatcher is not None:
-            (callable, match) = dispatcher
-            response = callable(self, request)
-            # Try to autocomplete the content-type header if not set
-            # explicitly.
-            # If there's no accept from the client and there's only one
-            # possible type from the match then use that as the best match.
-            # Otherwise use mimeparse to work out what the best match was. If
-            # the best match if not a wildcard then we know what content-type
-            # should be.
-            if isinstance(response, http.Response) and \
-                    not response.headers.get('content-type'):
-                accept = str(request.accept)
-                if not accept and len(match['accept']) == 1:
-                    best_match = match['accept'][0]
-                else:
-                    best_match = mimeparse.best_match(match['accept'], accept)
-                if '*' not in best_match:
-                    response.headers['content-type'] = best_match
-            return response
-        # No match, send 406
-        return http.not_acceptable([('Content-Type', 'text/plain')], \
-                                   '406 Not Acceptable')
-
-
-def _best_dispatcher(dispatchers, request):
-    """
-    Find the best dispatcher for the request.
-    """
-    # Use content negotation to filter the dispatchers to an ordered list of
-    # only those that match.
-    if request.headers.get('content-type'):
-        dispatchers = _filter_dispatchers_on_content_type(dispatchers, request)
-    if request.headers.get('accept'):
-        dispatchers = _filter_dispatchers_on_accept(dispatchers, request)
-    # Return the best match or None
-    if dispatchers:
-        return dispatchers[0]
+def child(matcher=None, klass=None):
+    if klass is None and not isinstance(matcher, _metaResource):
+        """ Child decorator used for finding child resources """
+        def decorator(func, matcher=matcher):
+            # No matcher? Use the function name.
+            if matcher is None:
+                matcher = func.__name__
+            # If the matcher is a string then create a TemplateChildMatcher in its
+            # place.
+            if isinstance(matcher, str):
+                matcher = TemplateChildMatcher(matcher)
+            # Annotate the function.
+            setattr(func, _RESTISH_CHILD, matcher)
+            # Return the function (unwrapped).
+            return func
+        return decorator
     else:
-        return None
-
-def _filter_dispatchers_on_content_type(dispatchers, request):
-    # Build an ordered list of the supported types.
-    supported = []
-    for d in dispatchers:
-        supported.extend(d[1]['content_type'])
-    # Find the best type.
-    best_match = mimeparse.best_match(supported, \
-                                      str(request.headers['content-type']))
-    # Return the matching dispatchers
-    return [d for d in dispatchers if best_match in d[1]['content_type']]
-
-
-def _filter_dispatchers_on_accept(dispatchers, request):
-    # Build an ordered list of the supported types.
-    supported = []
-    for d in dispatchers:
-        supported.extend(d[1]['accept'])
-    # Find the best accept type
-    best_match = mimeparse.best_match(supported, str(request.accept))
-    # Return the matching dispatchers
-    return [d for d in dispatchers if best_match in d[1]['accept']]
-
-
-def child(matcher=None):
-    """ Child decorator used for finding child resources """
-    def decorator(func, matcher=matcher):
-        # No matcher? Use the function name.
-        if matcher is None:
-            matcher = func.__name__
-        # If the matcher is a string then create a TemplateChildMatcher in its
-        # place.
+        if klass is None:
+            klass = matcher
+            matcher = None
+        
+        def func(self, request, segments, *args, **kwargs):
+            return klass(*args, **kwargs), segments
+        
         if isinstance(matcher, str):
             matcher = TemplateChildMatcher(matcher)
-        # Annotate the function.
+        
         setattr(func, _RESTISH_CHILD, matcher)
-        # Return the function (unwrapped).
+        setattr(func, _RESTISH_CHILD_CLASS, klass)
         return func
-    return decorator
-
 
 class TemplateChildMatcher(object):
     """
@@ -326,4 +186,168 @@ def _normalise_mimetype(mimetype):
         return real
     # Oh well.
     return mimetype
+
+
+class _metaResource(type):
+    """
+    Resource meta class that gathers all annotations for easy access.
+    """
+    def __new__(cls, name, bases, clsattrs):
+        cls = type.__new__(cls, name, bases, clsattrs)
+        _gather_request_dispatchers(cls, clsattrs)
+        _gather_child_factories(cls, clsattrs)
+        return cls
+
+
+def _gather_request_dispatchers(cls, clsattrs):
+    """
+    Gather any request handler -annotated methods and add them to the class's
+    request_dispatchers attribute.
+    """
+    # Copy the super class's 'request_dispatchers' dict (if any) to this class.
+    cls.request_dispatchers = dict(getattr(cls, 'request_dispatchers', {}))
+    for callable in _find_annotated_funcs(clsattrs, _RESTISH_METHOD):
+        method = getattr(callable, _RESTISH_METHOD, None)
+        match = getattr(callable, _RESTISH_MATCH)
+        cls.request_dispatchers.setdefault(method, []).append((callable, match))
+
+
+def _gather_child_factories(cls, clsattrs):
+    """
+    Gather any 'child' annotated methods and add them to the class's
+    child_factories attribute.
+    """
+    annotation = _RESTISH_CHILD
+    # Copy the super class's 'child_factories' list (if any) to this class.
+    cls.child_factories = list(getattr(cls, 'child_factories', []))
+    # Extend child_factories to include the ones found on this class.
+    for name, func in clsattrs.iteritems():
+        if hasattr(func, annotation) and getattr(func, annotation, None) is None:
+            setattr(func, annotation, TemplateChildMatcher(name))
+    
+    child_factories = _find_annotated_funcs(clsattrs, annotation)
+    cls.child_factories.extend((getattr(f, annotation), f)
+                               for f in child_factories)
+    # Sort the child factories by score.
+    cls.child_factories = sorted(cls.child_factories,
+                                 key=lambda i: i[0].score, reverse=True)
+
+
+def _find_annotated_funcs(clsattrs, annotation):
+    """
+    Return a (generated) list of methods that include the given annotation.
+    """
+    
+    funcs = (item for item in clsattrs.itervalues() \
+             if inspect.isroutine(item))
+    funcs = (func for func in funcs \
+             if getattr(func, annotation, None) is not None)
+    return funcs
+
+
+class Resource(object):
+    """
+    Base class for additional resource types.
+
+    Provides the basic API required of a resource (resource_child(request,
+    segments) and __call__(request)), possibly dispatching to annotated methods
+    of the class (using metaclass magic).
+    """
+
+    __metaclass__ = _metaResource
+    
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def resource_child(self, request, segments):
+        for matcher, func in self.child_factories:
+            match = matcher(request, segments)
+            if match is not None:
+                break
+        else:
+            return None
+        match_args, match_kwargs, segments = match
+        result = func(self, request, segments, *match_args, **match_kwargs)
+        if result is None:
+            return None
+        elif isinstance(result, tuple):
+            return result
+        else:
+            return result, segments
+
+    def __call__(self, request):
+        # Get the dispatchers for the request method.
+        dispatchers = self.request_dispatchers.get(request.method)
+        # No normal dispatchers for method found,
+        if dispatchers is None:
+            # Looking for a magic dispatcher
+            dispatchers = self.request_dispatchers.get(ALL.method)
+            # No magic dispatchers found either,
+            # send 405 with list of allowed methods.
+            if dispatchers is None:
+                return http.method_not_allowed(', '.join(self.request_dispatchers))
+        # Look up the best dispatcher
+        dispatcher = _best_dispatcher(dispatchers, request)
+        if dispatcher is not None:
+            (callable, match) = dispatcher
+            response = callable(self, request)
+            # Try to autocomplete the content-type header if not set
+            # explicitly.
+            # If there's no accept from the client and there's only one
+            # possible type from the match then use that as the best match.
+            # Otherwise use mimeparse to work out what the best match was. If
+            # the best match if not a wildcard then we know what content-type
+            # should be.
+            if isinstance(response, http.Response) and \
+                    not response.headers.get('content-type'):
+                accept = str(request.accept)
+                if not accept and len(match['accept']) == 1:
+                    best_match = match['accept'][0]
+                else:
+                    best_match = mimeparse.best_match(match['accept'], accept)
+                if '*' not in best_match:
+                    response.headers['content-type'] = best_match
+            return response
+        # No match, send 406
+        return http.not_acceptable([('Content-Type', 'text/plain')], \
+                                   '406 Not Acceptable')
+
+
+def _best_dispatcher(dispatchers, request):
+    """
+    Find the best dispatcher for the request.
+    """
+    # Use content negotation to filter the dispatchers to an ordered list of
+    # only those that match.
+    if request.headers.get('content-type'):
+        dispatchers = _filter_dispatchers_on_content_type(dispatchers, request)
+    if request.headers.get('accept'):
+        dispatchers = _filter_dispatchers_on_accept(dispatchers, request)
+    # Return the best match or None
+    if dispatchers:
+        return dispatchers[0]
+    else:
+        return None
+
+def _filter_dispatchers_on_content_type(dispatchers, request):
+    # Build an ordered list of the supported types.
+    supported = []
+    for d in dispatchers:
+        supported.extend(d[1]['content_type'])
+    # Find the best type.
+    best_match = mimeparse.best_match(supported, \
+                                      str(request.headers['content-type']))
+    # Return the matching dispatchers
+    return [d for d in dispatchers if best_match in d[1]['content_type']]
+
+
+def _filter_dispatchers_on_accept(dispatchers, request):
+    # Build an ordered list of the supported types.
+    supported = []
+    for d in dispatchers:
+        supported.extend(d[1]['accept'])
+    # Find the best accept type
+    best_match = mimeparse.best_match(supported, str(request.accept))
+    # Return the matching dispatchers
+    return [d for d in dispatchers if best_match in d[1]['accept']]
 
